35e11a541b820d71dac6f24049c48acf32afb0b6
AndreLewin <14772765+AndreLewin@users.noreply.github.com>
2023-01-02 15:21:22 +0100

    detect when the cell should be colored by the pencil tool

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,17 +1,46 @@
+import { useCallback } from "react"
+import store from "../store"
 import { Cell } from "./Grid"
 
 const CellComponent: React.FC<{ cell: Cell }> = ({ cell }) => {
+  const selectedTool = store(state => state.selectedTool)
+  const selectedColor = store(state => state.selectedColor)
+
+  const handleMouseOver = useCallback<any>((event: MouseEvent) => {
+    if (selectedTool === "pencil") {
+      const { buttons } = event
+      // 1 === the left button was held
+      if (buttons === 1) {
+        if (cell.color !== selectedColor) {
+          console.log("color that cell")
+        }
+      }
+    }
+  }, [cell, selectedTool, selectedColor])
+
+  const handleMouseDown = useCallback<any>((event: MouseEvent) => {
+    if (selectedTool === "pencil") {
+      if (cell.color !== selectedColor) {
+        console.log("color that cell")
+      }
+    }
+  }, [cell, selectedTool, selectedColor])
 
   return (
     <>
       <div
+        className="container"
         style={{ "backgroundColor": cell.color }}
+        onMouseOver={(e) => handleMouseOver(e)}
+        onMouseDown={(e) => handleMouseDown(e)}
       >
         WIP
       </div>
       <style jsx>
         {`
-          
+          .container {
+            border: dashed 0.5px rgba(0,0,0,0.5)
+          }
         `}
       </style>
     </>
