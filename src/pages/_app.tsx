aaa248f8bdb4f686c96b738f1b93cfe401422568
AndreLewin <14772765+AndreLewin@users.noreply.github.com>
2023-01-31 08:44:58 +0100

    add some Head metadata

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -23,8 +23,10 @@ const MyApp: AppType<{ session: Session | null }> = ({
   return (
     <>
       <Head>
-        <title>Page title</title>
+        <title>Emoji Adventure</title>
+        <meta content="Create your own interactive adventure" />
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>😀</text></svg>"> </link>
       </Head>
 
       {!isClientSide ? null :
