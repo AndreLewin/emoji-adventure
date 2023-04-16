bcc7c773ac9ba6e575ee2274f78c4269429f8be5
AndreLewin <14772765+AndreLewin@users.noreply.github.com>
2023-04-16 16:12:35 +0200

    try export with process.cwd() for path building

--- src/pages/api/export/[adventureId].ts
+++ src/pages/api/export/[adventureId].ts
@@ -16,24 +16,37 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
 
   // put all files from public/exportTemplate inside it (except index.html, because we will change it)
   const addFile = (filePath: any) => {
-    if (filePath === `./public/exportTemplate/index.html`) return
+    console.log("filePath | [adventureId].ts l19", filePath)
+
+    if (filePath === (process.cwd() + `/public/exportTemplate/index.html`)) return
 
     const stats = fs.statSync(filePath);
+    console.log("stats | [adventureId].ts l24", stats)
+
 
     if (stats.isFile()) {
       const data = fs.readFileSync(filePath);
-      folder.file(path.relative('./public/exportTemplate', filePath), data);
+      const relativePath = path.relative(process.cwd() + '/public/exportTemplate', filePath);
+      console.log("relativePath | [adventureId].ts l26", relativePath)
+
+      folder.file(relativePath, data);
     } else if (stats.isDirectory()) {
-      const subFolder = folder.folder(path.relative('./public/exportTemplate', filePath));
-      for (const file of fs.readdirSync(filePath)) {
+      const relativePath = path.relative(process.cwd() + '/public/exportTemplate', filePath);
+      console.log("relativePath | [adventureId].ts l31", relativePath)
+
+      const subFolder = folder.folder(relativePath);
+      const subFiles = fs.readdirSync(filePath);
+      console.log("subFiles | [adventureId].ts l39", subFiles)
+
+      for (const file of subFiles) {
         addFile(path.join(filePath, file));
       }
     }
   };
-  addFile('./public/exportTemplate');
+  addFile(process.cwd() + '/public/exportTemplate');
 
   // add index.html to the virtual folder using the data of the exported adventure
-  const oldIndexString = fs.readFileSync('./public/exportTemplate/index.html', 'utf8');
+  const oldIndexString = fs.readFileSync(process.cwd() + '/public/exportTemplate/index.html', 'utf8');
   // TODO: check on ownership
   // TODO: fetch adventure (cf api/adventure/[adventureId].ts)
   // TODO: replace title, description and script of index.html
