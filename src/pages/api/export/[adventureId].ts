b51522f0f6ab84ac63481325c9759083d5993caf
AndreLewin <14772765+AndreLewin@users.noreply.github.com>
2023-04-17 20:40:05 +0200

    everyone can now export their adventure!

--- src/pages/api/export/[adventureId].ts
+++ src/pages/api/export/[adventureId].ts
@@ -1,13 +1,26 @@
 import { type NextApiRequest, type NextApiResponse } from "next";
 import fs from 'fs';
-import { Readable } from "stream";
 import JSZip from "jszip";
-import path from 'path';
-
+import { unstable_getServerSession } from "next-auth/next";
+import { authOptions } from "../auth/[...nextauth]";
+import { prisma } from "../../../server/db/client"
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
   const { query, method, body } = req
   const adventureId = query.adventureId as string
+
+  const session = await unstable_getServerSession(req, res, authOptions)
+  const userId = session?.user?.id ?? null
+
+  const adventure = await prisma.adventure.findFirst({
+    where: {
+      id: adventureId,
+      // you can export only your adventure
+      userId: userId ?? ""
+    }
+  });
+
+  if (adventure === null) return res.status(400).json({ error: 'Could not export adventure' })
 
   fs.readFile(process.cwd() + `/public/exportTemplate.zip`, function (err, data) {
     if (err) throw err;
@@ -15,10 +28,15 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
     JSZip.loadAsync(data).then(async function (zip) {
       const oldIndexHtmlString = await zip.file("index.html")!.async("string");
 
-      // TODO: check on ownership
-      // TODO: fetch adventure (cf api/adventure/[adventureId].ts)
-      // TODO: replace title, description and script of index.html
-      const newIndexHtmlString = oldIndexHtmlString.replace(/DEMO-ADVENTURE/g, adventureId);
+      // replace adventure
+      let replacement = JSON.stringify(JSON.stringify(adventure));
+      replacement = replacement.substring(1, replacement.length - 1);
+      let newIndexHtmlString = oldIndexHtmlString.replace(/\{\\"adventure\\":(.*)\}\]\}\]\}\]/, `{\\"adventure\\":${replacement}}]}]}]`);
+      // replace name
+      newIndexHtmlString = newIndexHtmlString.replace(/DEMO-ADVENTURE/g, adventure.name ?? "Name");
+      // replace description
+      newIndexHtmlString = newIndexHtmlString.replace(/DEMO-DESCRIPTION/g, adventure.description ?? "Description");
+
       zip.file('index.html', newIndexHtmlString);
 
       const data = await zip.generateAsync({ type: 'nodebuffer' });
