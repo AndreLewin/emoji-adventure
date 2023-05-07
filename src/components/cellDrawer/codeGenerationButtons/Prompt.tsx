a07901428a4a922d938249e820b1b3a54e342fac
AndreLewin <14772765+AndreLewin@users.noreply.github.com>
2023-05-07 13:16:28 +0200

    can force valid Prompt answer

--- src/components/cellDrawer/codeGenerationButtons/Prompt.tsx
+++ src/components/cellDrawer/codeGenerationButtons/Prompt.tsx
@@ -1,4 +1,4 @@
-import { Button, Modal, Textarea } from "@mantine/core"
+import { Button, Checkbox, Modal, Textarea } from "@mantine/core"
 import { useCallback, useState } from "react"
 import { getHotkeyHandler } from '@mantine/hooks'
 import store from "../../../store"
@@ -9,14 +9,33 @@ const Prompt: React.FC<{ gridId: number, cellIndex: number }> = ({ gridId, cellI
   const [textToDisplay, setTextToDisplay] = useState<string>("")
   const updateCellWithAppend = store(state => state.updateCellWithAppend)
   const activeCScriptTab = store(state => state.activeCScriptTab)
+  const [shouldForceValidAnswer, setShouldForceValidAnswer] = useState<boolean>(!!(localStorage.getItem("shouldForceValidAnswer")))
 
   const handlePrompt = useCallback<any>(() => {
-    const script = `const answer = #p(\`${textToDisplay}\`)\nif (answer === "") {\n  \n}`
+    const scriptNotForced = `const answer = #p(\`${textToDisplay}\`)
+if (answer === "1") {
+  
+}
+if (answer === "2") {
+  
+}
+`
+    const scriptForced = `while (true) {
+  const answer = #p(\`${textToDisplay}\`)
+  if (answer === "1") {
+    
+    break
+  }
+  if (answer === "2") {
+    
+    break
+  }
+}`
     updateCellWithAppend({
       gridId,
       cellIndex,
       cellUpdate: {
-        [activeCScriptTab]: script
+        [activeCScriptTab]: shouldForceValidAnswer ? scriptForced : scriptNotForced
       }
     })
     setIsModalOpened(false)
@@ -25,7 +44,7 @@ const Prompt: React.FC<{ gridId: number, cellIndex: number }> = ({ gridId, cellI
       const codeEditor = window.document.querySelector(".npm__react-simple-code-editor__textarea") as HTMLElement
       codeEditor?.focus()
     }, 50);
-  }, [activeCScriptTab, updateCellWithAppend, textToDisplay, gridId, cellIndex])
+  }, [activeCScriptTab, updateCellWithAppend, textToDisplay, gridId, cellIndex, shouldForceValidAnswer])
 
   return (
     <>
@@ -45,6 +64,11 @@ const Prompt: React.FC<{ gridId: number, cellIndex: number }> = ({ gridId, cellI
             onKeyDown={getHotkeyHandler([
               ['ctrl+Enter', handlePrompt]
             ])}
+          />
+          <Checkbox
+            checked={shouldForceValidAnswer}
+            onChange={() => { localStorage.setItem("shouldForceValidAnswer", "true"), setShouldForceValidAnswer(!shouldForceValidAnswer) }}
+            label="Force valid answer"
           />
           <Button
             onClick={handlePrompt}
