4d8c17e324b7daa7daf81fe210e5218fe78102be
AndreLewin <14772765+AndreLewin@users.noreply.github.com>
2023-05-17 09:00:25 +0200

    music example

--- src/components/cellDrawer/codeGenerationButtons/Music.tsx
+++ src/components/cellDrawer/codeGenerationButtons/Music.tsx
@@ -7,7 +7,36 @@ const Music: React.FC<{ gridId: number, cellIndex: number }> = ({ gridId, cellIn
   const updateCellWithAppend = store(state => state.updateCellWithAppend)
 
   const handleClick = useCallback<any>(() => {
-    const script = `#.animalAudio = new Audio('https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3')\n\n#.roar = () => {\n  #.animalAudio.play()\n}\n\n// After running the code above (for example in an On Init script), you can use the following line every time you want to play the sound\n#.roar()\n`
+    const script = `// play music (automatically loops)
+_playMusic("https://cdn.pixabay.com/audio/2023/04/03/audio_6700efb297.mp3")
+
+// warning: if you put _playMusic in On Init or in the On View of the first grid, it might not work. This is because web browsers prevent audio to be played on websites where the user still has not interacted (clicked)
+
+// On View is a good place to put _playMusic
+// if the provided URL string is the same as the already active music, the music will just continue
+// you can see the active music that way:
+_l(_activeMusic)
+
+// play an other music after some time (only one music can be played at the same time)
+await _sleep(3000)
+_playMusic("https://cdn.pixabay.com/audio/2023/03/17/audio_555bd9964f.mp3")
+
+// pause the music
+_pauseMusic()
+
+// resume the music where it was paused
+_resumeMusic()
+
+// stop the music
+_playMusic("")
+
+// restart the music from the start
+_playMusic("")
+_playMusic("https://cdn.pixabay.com/audio/2023/03/17/audio_555bd9964f.mp3")
+
+// you can find free to use music in https://pixabay.com/music/search/
+// open the Network tab of your web browser to find the URL of the mp3 files    
+`
     updateCellWithAppend({
       gridId,
       cellIndex,
