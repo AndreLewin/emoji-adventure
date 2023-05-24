cf7ea4022a61381a5c277dde0c0d9ba4a3ac7ecf
AndreLewin <14772765+AndreLewin@users.noreply.github.com>
2023-05-24 17:56:24 +0200

    scale Viewers down if width < 400px (so looks good vertically on some mobile)

--- src/components/viewers/Viewers.tsx
+++ src/components/viewers/Viewers.tsx
@@ -1,18 +1,36 @@
+import { useViewportSize } from "@mantine/hooks"
 import DisplayText from "../DisplayText"
 import HoveringTextSpans from "../HoveringTextSpans"
 import GridInfoViewer from "./GridInfoViewer"
 import GridViewer from "./GridViewer"
+import { useMemo } from "react"
 
 const Viewers: React.FC<{}> = ({ }) => {
+  const { height, width } = useViewportSize()
+
+  // make sure the whole grid (and the rest) is visible even if width < 400px
+  // https://css-tricks.com/scaled-proportional-blocks-with-css-and-javascript/
+  const scale = useMemo<number>(() => {
+    if (width >= 400) return 1
+    return width / 400
+  }, [width])
 
   return (
-    <div>
+    <div className="viewer-container" style={{ transform: `scale(${scale})` }}>
       <GridViewer />
       <GridInfoViewer />
       <DisplayText />
       <div style={{ "position": "absolute", "top": "0px", "left": "0px", "pointerEvents": "none" }}>
         <HoveringTextSpans />
       </div>
+
+      <style jsx>
+        {`
+          .viewer-container {
+            transform-origin: 0px 0px;
+          }
+        `}
+      </style>
     </div>
   )
 }
